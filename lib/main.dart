000f759493612b2c7c6aa3f0ce34a8f6156d78ab
Davis Diaz <davis.diaz@gmail.com>
2022-11-04 20:34:20 -0500

    se inicia con la aplicacion de autenticacion firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:app_libros/firebase_options.dart';
 import 'package:app_libros/pages/login_page.dart';
 import 'package:app_libros/pages/register_page.dart';
 import 'package:app_libros/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main()  async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   runApp(const MyApp());
 }
 
